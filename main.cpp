541082d4241183fab2a32ee57562821162454bb5
Abhishek Pant <abhishek.pant93@gmail.com>
2015-02-09 00:05:15 +0530

    blur input image

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/core/core.hpp"
+#include <string>
 
 #include "Superpixels.hpp"
 
@@ -24,13 +25,18 @@ int main(int argc, char* argv[]){
     
     imshow("Original", img);
 
+    blur(img, img, Size(3,3));
+    imshow("Blurred", img);
+    
     Superpixels sp(img);
     
     Mat boundaries = sp.viewSuperpixels();
     imshow("Superpixel boundaries", boundaries);
-
+    imwrite("boundaries.png", boundaries);
+    
     Mat recolored = sp.colorSuperpixels();
     imshow("Average superpixel colors", recolored);
+    imwrite("recolored.png", recolored);
     
     waitKey(0);
 
